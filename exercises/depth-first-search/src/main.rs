49d047298ed0763ca3fb82e8a878cc6c63d8c5cb
Ricardo <ricardoervilha456@gmail.com>
2023-09-13 14:37:23 -0300

    Compilou.

--- exercises/depth-first-search/src/main.rs
+++ exercises/depth-first-search/src/main.rs
@@ -3,180 +3,181 @@ use std::io;
 
 struct Node {
     able_actions: Vec<i32>, // Ações disponíveis
-    already_attempt: i32,   // Última ação tentada
+    action: i32,            // Última ação tentada
 }
 
 struct Pilha {
-    pilha: Vec<Node>,
+    pilha : Vec<Node> //Armazena os nós, ou seja, pontos onde eu posso ir para um lugar
 }
 
-// Pilha de nós
-impl Pilha {
-    // Construtor, cria e retorna uma fronteira vazia.
+impl Pilha{
+
+    //Construtor
     fn new() -> Self {
-        Pilha { pilha: Vec::new() }
+        Pilha { pilha : Vec::new() }
     }
 
-    // Adiciona elemento na fronteira.
-    fn add(&mut self, node: &'static mut Node) {
+    //Adiciona elementos na pilha
+    fn add(&mut self, node : Node){
         self.pilha.push(node);
     }
 
-    // Verifica se a fronteira está vazia
+    //Verifica se a pilha está vazia
     fn vazia(&self) -> bool {
         self.pilha.is_empty()
     }
 
-    fn top(&mut self) -> Option<&mut Node> {
-        self.pilha.last_mut().map(|x| *x)
-    }
-
-    // Remove elemento da fronteira e retorna o valor
-    fn remove(&mut self) -> Option<&'static mut Node> {
-        if self.pilha.is_empty() {
-            println!("[ERRO] Removendo elemento com fronteira vazia!");
+    //Remove um elemento do topo da pilha e retorna o valor
+    fn remove(&mut self){
+        if self.vazia() {
+            println!("[ERRO] Remoção de elemento de uma pilha vazia!");
             process::exit(1);
-        } else {
-            self.pilha.pop()
+        } else{
+            self.pilha.pop();
         }
     }
-}
 
-fn inicializa_matriz(matriz: &mut Vec<Vec<i32>>, n: usize) {
-    for i in 0..n {
-        matriz[i][0] = (i + 1) as i32;
+    fn top(&mut self) -> Option<&mut Node> {
+        self.pilha.last_mut()
     }
 }
 
-fn imprime_matriz(matriz: &Vec<Vec<i32>>, n: usize) {
-    for i in 0..n {
-        for j in 0..n {
+fn inicializa_matriz(matriz : &mut Vec<Vec<i32>>, n : usize){
+    // Preencho a primeira coluna
+    for i in 0..n{
+        matriz[i][0] = (i+1) as i32;
+    }
+}
+
+fn imprime_matriz(matriz : &Vec<Vec<i32>>, n : usize){
+    for i in 0..n{
+        for j in 0..n{
             print!("{:3}", matriz[i][j]);
         }
-        println!();
+        println!("");
     }
 }
 
-fn preenche_regras(matriz: &Vec<Vec<i32>>, n: i32, i: usize, j: usize) -> Vec<i32> {
-    // Para cada regra
-    let mut rules: Vec<i32> = Vec::new();
-    for r in 1..n + 1 {
+fn preenche_regras(matriz: &Vec<Vec<i32>>, n : usize, i : usize, j : usize) -> Vec<i32> {
+
+    //Para cada regra 
+    let mut rules : Vec<i32> = Vec::new();
+
+    //Para cada regra possível 
+    for r in 1..n+1 {
         let mut flag = true;
 
-        // Testar para coluna
-        for x in 0..i {
-            if matriz[x][j] == r {
+        //Testar para coluna
+        for x in 0..i{
+            if matriz[x][j] == (r as i32) {
                 flag = false;
                 break;
             }
         }
 
-        if flag {
-            // Testar para linha
-            for y in 0..j {
-                if matriz[i][y] == r {
+        if flag{
+            
+            //Testar para linha
+            for y in 0..j{
+                if matriz[i][y] == (r as i32){
                     flag = false;
                     break;
                 }
             }
         }
 
+        //Adiciono na lista de regras que podem ser utilizadas 
         if flag {
-            rules.push(r);
+            rules.push(r as i32);
         }
-    }
+    } 
 
     return rules;
 }
 
-fn dfs(matriz: &mut Vec<Vec<i32>>, n: i32) {
-    let mut i: usize = 0;
-    let mut j: usize = 1;
+//Função para executar a busca em profundidade
+fn dfs(matriz: &mut Vec<Vec<i32>>, n : usize){
+    //Ponto de partida
+    let mut i : usize = 0;
+    let mut j : usize = 1;
+
+    //Pilha para armazenar as decisões
     let mut pilha = Pilha::new();
+    
+    let mut backtracking = false;
+    loop{
+        if i < n && j < n {
 
-    let mut cond = true;
-    loop {
-        if i < n as usize && j < n as usize {
-            let mut regras_disponiveis: Vec<i32>;
-
-            if cond {
-                regras_disponiveis = preenche_regras(&matriz, n, i, j);
-
-                // Estado de impasse
-                if regras_disponiveis.is_empty() {
-                    // Troco cond pra falso para não preencher regras novamente
-                    cond = false;
-                } else {
-                    // Salvo esse estado
-                    let regra = regras_disponiveis.pop();
-                    let no = Node {
-                        able_actions: regras_disponiveis.clone(), // Clone para evitar problemas de mutabilidade
-                        already_attempt: regra.unwrap(),
+            if !backtracking {
+                let mut regras_disponives = preenche_regras(&matriz, n, i, j);
+            
+                if !regras_disponives.is_empty() {
+                    let action = regras_disponives.remove(0);
+                    let node = Node{
+                        able_actions : regras_disponives,
+                        action : action
                     };
-                    pilha.add(Box::leak(Box::new(no))); // Box::leak para armazenar uma referência estática
-                    matriz[i][j] = regra.unwrap();
-
-                    // Atualizo para ir aprofundando na árvore
-                    if i < n as usize {
+                    pilha.add(node);
+                    matriz[i][j] = action;
+                    if i < n-1{
                         i += 1;
-                    } else {
-                        i = 0;
-                        j += 1;
+                    }else{
+                        i = 0; j += 1;
+                    }
+                }else{
+                    backtracking = true; //Estou em estado de backtracking 
+                    if i > 0{
+                        i -= 1;
+                    }else{
+                        i = n - 1; j -= 1;
                     }
                 }
             } else {
-                if !pilha.vazia() {
-                    // Clona o nó superior da pilha
-                    let mut top = pilha.top().unwrap().clone();
-
-                    // Atualiza o pai para tentar a próxima regra que ele permite
-                    let regra = top.able_actions.pop();
-                    top.already_attempt = regra.unwrap();
-                    matriz[i][j] = regra.unwrap();
-                    cond = true;
-
-                    if i < n as usize {
+                let top : &mut Node = pilha.top().unwrap(); //Pego o topo da pilha
+    
+                if top.able_actions.is_empty() {
+                    pilha.remove(); //Removo o topo da pilha
+                    if i > 0{
+                        i -= 1;
+                    }else{
+                        i = n - 1; j -= 1;
+                    }
+                }else{
+                    //Para não calcular regras disponives de novo
+                    let action = top.able_actions.remove(0);
+                    top.action = action;
+                    matriz[i][j] = action;
+                    if i < n-1{
                         i += 1;
-                    } else {
-                        i = 0;
-                        j += 1;
+                    }else{
+                        i = 0; j += 1;
                     }
-                } else {
-                    matriz[i][j] = 0;
-                    if !pilha.vazia() {
-                        pilha.remove();
-                        if i > 0 {
-                            i -= 1;
-                        } else {
-                            i = n as usize - 1;
-                            j -= 1;
-                        }
-                    } else {
-                        println!("Nenhuma solução encontrada.");
-                        process::exit(1);
-                    }
+                    backtracking = false; //Sai do estado de backtracking
                 }
             }
-        } else {
-            break;
+    
+        }else{
+            println!("i: {}, j: {}", i, j);
+            println!("Solução encontrada");
+            imprime_matriz(&matriz, n);
+            process::exit(1);
         }
+        
     }
+    
 }
 
-fn main() {
+fn main(){
     println!("*-*-* Quadrado Latino *-*-*");
     println!("Digite o valor de n: ");
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Falha ao ler o valor de n!");
+    io::stdin().read_line(&mut input).expect("Falha ao ler o valor do n!");
 
-    // Conversão para inteiro da string lida
-    let n: i32 = input.trim().parse().unwrap();
+    let n : i32 = input.trim().parse().unwrap();
 
-    // Criação da matriz dinâmica nxn preenchida com zeros
-    let mut matriz: Vec<Vec<i32>> = vec![vec![0; n as usize]; n as usize];
+    let mut matriz : Vec<Vec<i32>> = vec![vec![0; n as usize]; n as usize];
 
     inicializa_matriz(&mut matriz, n as usize);
-
-    dfs(&mut matriz, n);
-    imprime_matriz(&matriz, n as usize);
+    
+    dfs(&mut matriz, n as usize)
 }
